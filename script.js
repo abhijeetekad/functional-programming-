b06ccccf2d2c8f93a0f29f97a816327f15eff0eb
Abhijeet Ekad <70329145+abhijeetekad@users.noreply.github.com>
2022-02-23 16:25:31 +0000

    js questions

--- script.js
+++ script.js
@@ -1,41 +1,141 @@
-// const numArr = [1,3,5,2,22,11,9,12];
+// 1. Given an array. Write a function to change all even numbers in an array to odd numbers by adding 1 to it.
 
-// // const incrementByTwo = num => num+2;
-// // let newArr = numArr.map(incrementByTwo)
+// ```jsx
+// Your output should be:
+// [1, 3, 3, 59, 5, 7, 25, 9, 15, 5]
+// ```
+// const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
 
-// const newArr = numArr.reduce((acc,curr)=>{
-//   // console.log(curr)
-//   console.log(acc.even+curr)
-//   if(curr%2==0){
-//     return {...acc,even:acc.even+curr}
-//   }else{
-//     return {...acc,odd:acc.odd+curr};
+// const oddArr = arr.map((data)=>data%2===0?data+1:data);
+// console.log(oddArr)
+//2. Get the names in an array for only those who have a cycle.
+// Your output should be: ['Tanay', 'Tanvi']
+// const family = [
+//   {
+//     name    : 'Tanay',
+//     haveCycle : true
+//   },
+//   {
+//     name     : 'Akanksha',
+//     haveCycle : false
+//   },
+//   {
+//     name     : 'Tanvi',
+//     haveCycle : true
+//   },
+// 	{
+//     name     : 'Kanak',
+//     haveCycle : false
 //   }
-// },{even:0,odd:0})
-// console.log(newArr);
+// ];
+// const getName = family.reduce((acc,curr)=>{
+//   if(curr.haveCycle){
+//     return [...acc,curr.name]
+//   }
+//   return acc;
+// },[])
+// console.log(getName)
+// 3. Given an array. ****Write a function that takes in the given array and prints only the numbers which are less than 8 and also an even number.
 
-// const newArr = numArr.reduce((acc, curr)=>curr%2===0?{...acc,even:acc.even+curr}:{...acc,odd:acc.odd+curr},{even:0,odd:0});
-// console.log(newArr);
+// ```jsx
+// const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
 
-//  const takeName = (name) =>{
-//   //  console.log(name)
-//    return addMsg(name);
-//  }
-//  const addMsg = (name) =>{
-//    return `${name} says hello`;
-//  }
-//   const nameLogger = (name) => msg => console.log(`${name} says ${msg}`)
-//   //  return addMsg(name);
-//   const abhijeetLogger = nameLogger("Abhijeet")
-//   // console.log(nameLogger)
-// abhijeetLogger("hello says")
+// Your output should be: [2, 6, 8, 4]
+// ```
+// const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+// // const newArr = arr.filter((data)=>{
+// //   if(data%2===0 && data<=8){
+// //     return data;
+// //   }
+// // })
 
-// nameLogger("raj")
+// console.log(newArr)
+// 8. Given an array. Convert it in to an object with key as the index of each element and value as the element itself.
 
-// const takeName = (name) => msg => console.log(`${name} says ${msg}`);
-// const nameLogger = takeName("abhijeet");
-// nameLogger("hello") 
+// ```jsx
+// const arr = ["You", "all", "are", "rockstars"];
 
-const textWithName = (msg) =>{
-  return 
-}
+// Your output should be:
+// {'0': "You", '1': "all", '2': "are", '3': "rockstars"}
+// ```
+
+// const arr = ["You", "all", "are", "rockstars"];
+
+
+// const newArr = arr.reduce((acc,curr,ind)=>{
+//   // console.log(curr)
+//   // console.log(ind)
+//   return {...acc,[ind]:curr}
+// },{});
+// console.log(newArr)
+// 11. Given an array. Write a function to join all elements of the array with a hyphen in between them.
+
+// ```jsx
+// const arr = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"]
+
+// Your output should be: 
+// Violet-Indigo-Blue-Green-Yellow-Orange-Red
+// const arr = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"]
+// const splitArr = arr.join("-")
+// console.log(splitArr)
+
+// 15. Flatten an array without using flat().
+// const input = [
+//     ['a', 'b', 'c'],
+//     ['c', 'd', 'e'],
+//     ['e', 'd', 'f'],
+//   ];
+// const newArr = input.reduce((acc,curr,ind)=>{
+//   // console.log(curr.ind)
+//   // console.log(ind)
+//   return [...acc, ...curr]
+// },[])
+// console.log(newArr)
+/*****************************/
+// * Return an object where each property is the name of the an ice cream flavor and each value is an integer that is the total count of that flavor.
+// * Store the returned data in a new iceCreamTotals variable.
+
+// const data = [
+//     { name: 'Superman', favoriteIceCreams: ['Strawberry', 'Vanilla', 'Chocolate', 'Cookies & Cream'] },
+//     { name: 'Batman', favoriteIceCreams: ['Cookies & Cream', 'Mint Chocolate Chip', 'Chocolate', 'Vanilla'] },
+//     { name: 'Flash', favoriteIceCreams: ['Chocolate', 'Rocky Road', 'Pistachio', 'Banana'] },
+//     { name: 'Aquaman', favoriteIceCreams: ['Vanilla', 'Chocolate', 'Mint Chocolate Chip'] },
+//     { name: 'Green Lantern', favoriteIceCreams: ['Vanilla', 'French Vanilla', 'Vanilla Bean', 'Strawberry'] },
+//     { name: 'Robin', favoriteIceCreams: ['Strawberry', 'Chocolate', 'Mint Chocolate Chip'] }
+// ];
+// const iceCreamTotals = data.reduce((acc,curr)=>{
+//   curr.favoriteIceCreams.map((iceCreamType)=>{
+//     if(acc[iceCreamType]){
+//       acc[iceCreamType] = ++acc[iceCreamType]
+//     }else{
+//       acc[iceCreamType]=1
+//     }
+//   })
+//   return acc;
+// },{})
+// console.log(iceCreamTotals)
+/*************************************/
+
+
+
+const arr = [
+  { id: 1, amount: 100 },
+  { id: 1, amount: 100 },
+  { id: 1, amount: 100 },
+  { id: 2, amount: 100 },
+  { id: 2, amount: 100 },
+];
+
+output : [{id:1,amount:300},{id:2,amount:200}] 
+const newArr = arr.reduce((acc,curr)=>{
+  let currentId = curr.id;
+  // console.log(currentId)
+  if(curr.id){
+    // return {.acc,curr.id}
+  }else{
+    
+  }
+  // return currentId
+  
+},[])
+console.log(newArr)
