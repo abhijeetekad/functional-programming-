cc7812da6e68d1a149262e105851437d9e8c27c4
abhijeetekad <ekadabhijeet4@gmail.com>
2023-03-08 15:34:12 +0530

    question and answers

--- Functional Programming Set 2/solutions.js
+++ Functional Programming Set 2/solutions.js
@@ -59,12 +59,12 @@
 // console.log(studentNames);
 // // Output: ["Anjali", "Arpit", "Ankit"]
 // Write an ES6 function which filters out the products which have a price greater than 40.
-const productData = [
-  { prodName: "Dairy Milk", price: 10 },
-  { prodName: "Dairy Milk Silk", price: 70 },
-  { prodName: "Five Star", price: 20 },
-  { prodName: "Mars", price: 50 },
-];
+// const productData = [
+//   { prodName: "Dairy Milk", price: 10 },
+//   { prodName: "Dairy Milk Silk", price: 70 },
+//   { prodName: "Five Star", price: 20 },
+//   { prodName: "Mars", price: 50 },
+// ];
 // // Your code here
 // const getProducts = productData.filter((data) => data.price > 40);
 
@@ -77,3 +77,73 @@ const productData = [
 
 // console.log(isDivisibleBy7);
 // // Output: 21
+// Write an ES6 function that takes an array of strings and returns the first string that is longer than 8 characters.
+// const names = [
+//   "Mohan",
+//   "Anjali",
+//   "Geetanjali",
+//   "Ankit",
+//   "Bhanu Kumar",
+//   "Ramakrishnan",
+//   "shayam",
+// ];
+// // Your code here
+// const isNamesGreaterThan8 = names.filter((data) => data.length > 8);
+
+// console.log(isNamesGreaterThan8[0]);
+// Output: "Geetanjali"
+// Write an ES6 function that takes an array of objects representing students with properties name and grade. Return the first student object that has a grade of "A".
+// const students = [
+//   { name: "John", grade: "B" },
+//   { name: "Mary", grade: "A" },
+//   { name: "Sam", grade: "C" },
+//   { name: "Sarah", grade: "A" },
+// ];
+
+// // Your code here
+// const findStudentWithGradeA = students.filter((data) => data.grade === "A");
+// console.log(findStudentWithGradeA[0]);
+// Output: { name: "Mary", grade: "A" }
+
+// Write an ES6 function that takes an array of objects representing students with properties name, grade and scholarship. Return the first student object that has a grade of "A" or they are a scholarship student.
+// const students = [
+//   { name: "John", grade: "B", scholarship: false },
+//   { name: "Mary", grade: "B", scholarship: true },
+//   { name: "Sam", grade: "A", scholarship: false },
+//   { name: "Sarah", grade: "A", scholarship: true },
+// ];
+
+// // Your code here
+// const student = students.filter(
+//   (data) => data.grade === "A" || data.scholarship === true
+// );
+
+// console.log(student[0]);
+// // Output: { name: "Mary", grade: "B", scholarship: true }
+// Write an ES6 function that takes an array of objects containing Bollywood movie information (title, director, year, rating) and returns an array with only the movie titles that were made before 1990 and has a rating above 8.0.
+// const bollywoodMovies = [
+//   { title: "Sholay", director: "Ramesh Sippy", year: 1975, rating: 8.2 },
+//   {
+//     title: "Amar Akbar Anthony",
+//     director: "Manmohan Desai",
+//     year: 1977,
+//     rating: 7.6,
+//   },
+//   { title: "Namak Halaal", director: "Prakash Mehra", year: 1982, rating: 7.4 },
+//   { title: "Mr. India", director: "Shekhar Kapur", year: 1987, rating: 7.8 },
+//   {
+//     title: "Qayamat Se Qayamat Tak",
+//     director: "Mansoor Khan",
+//     year: 1988,
+//     rating: 7.6,
+//   },
+//   { title: "Parinda", director: "Vidhu Vinod Chopra", year: 1989, rating: 8.1 },
+//   { title: "Dil", director: "Indra Kumar", year: 1990, rating: 7.8 },
+// ];
+
+// // Your code here
+// const bestOldMovies = bollywoodMovies.filter(
+//   (data) => data.year < 1990 && data.rating > 8.0
+// );
+// const nameListOfBollywoodMovies = bestOldMovies.map((data) => data.title);
+// console.log(nameListOfBollywoodMovies); // Output: ['Sholay', 'Parinda']
